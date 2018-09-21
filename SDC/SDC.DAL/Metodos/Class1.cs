f79b0b4aa56ee0c89ee9a8c2c3a4d075494b5c7a
harry <harry@HARRYMORA>
2018-09-21 10:50:18 -0600

    Creacion prop DAL

---
+++ SDC/SDC.DAL/Metodos/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDC.DAL.Metodos
+{
+    class Class1
+    {
+        public int MyProperty { get; set; }
+    }
+}
